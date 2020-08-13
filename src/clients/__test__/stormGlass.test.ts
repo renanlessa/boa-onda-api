3484ea74fd4fd330d1126ae2c522d2e906a33414
Renan Lessa <renanlessa@gmail.com>
2020-08-13 20:34:23 -0300

    prettier and setup node config

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -6,71 +6,69 @@ import stormGlassNormalizedWeather3HoursFixture from '@test/fixtures/stormglass_
 jest.mock('axios');
 
 describe('StormGlass client', () => {
-    
-    const mockedAxios = axios as jest.Mocked<typeof axios>;
-    
-    it('should return the normalized forecast from the StormGlass service', async() => {
-        const lat = -33.792726;
-        const lng = 151.289824;
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-        mockedAxios.get.mockResolvedValue({data: stormGlassWeather3HoursFixture});
+  it('should return the normalized forecast from the StormGlass service', async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
 
-        const stormGlass = new StormGlass(mockedAxios);
-        const response = await stormGlass.fetchPoints(lat, lng);
-        expect(response).toEqual(stormGlassNormalizedWeather3HoursFixture);
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture });
+
+    const stormGlass = new StormGlass(mockedAxios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+    expect(response).toEqual(stormGlassNormalizedWeather3HoursFixture);
+  });
+
+  it('should exclude incomplete data points', async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+    const incompleteResponse = {
+      hours: [
+        {
+          windDirection: {
+            noaa: 300,
+          },
+          time: '2020-04-26T00:00:00+00:00',
+        },
+      ],
+    };
+
+    mockedAxios.get.mockResolvedValue({ data: incompleteResponse });
+
+    const stormGlass = new StormGlass(mockedAxios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+
+    expect(response).toEqual([]);
+  });
+
+  it('should get a generic error from StormGlass service when the request fail before reaching the service', async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+
+    mockedAxios.get.mockRejectedValue({ message: 'Network Error' });
+
+    const stormGlass = new StormGlass(mockedAxios);
+
+    await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
+      'Unexpected error when trying to communicate to StormGlass: Network Error'
+    );
+  });
+
+  it('should get an StormGlassResponseError when the StormGlass service responds with error', async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+
+    mockedAxios.get.mockRejectedValue({
+      response: {
+        status: 429,
+        data: { errors: ['Rate Limit reached'] },
+      },
     });
 
-    it('should exclude incomplete data points', async() => {
-        const lat = -33.792726;
-        const lng = 151.289824;
-        const incompleteResponse = {
-            hours: [
-                {
-                    windDirection: {
-                        noaa: 300
-                    },
-                    time: '2020-04-26T00:00:00+00:00'
-                }
-            ]
-        };
+    const stormGlass = new StormGlass(mockedAxios);
 
-        mockedAxios.get.mockResolvedValue({data: incompleteResponse});
-
-        const stormGlass = new StormGlass(mockedAxios);
-        const response = await stormGlass.fetchPoints(lat, lng);
-
-        expect(response).toEqual([]);
-    });
-
-    it('should get a generic error from StormGlass service when the request fail before reaching the service', async() => {
-        const lat = -33.792726;
-        const lng = 151.289824;
-
-        mockedAxios.get.mockRejectedValue({message: 'Network Error'});
-
-        const stormGlass = new StormGlass(mockedAxios);
-        
-        await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
-            'Unexpected error when trying to communicate to StormGlass: Network Error'
-        );
-    });
-
-    it('should get an StormGlassResponseError when the StormGlass service responds with error', async() => {
-        const lat = -33.792726;
-        const lng = 151.289824;
-
-        mockedAxios.get.mockRejectedValue({
-            response: {
-                status: 429,
-                data: {errors: ['Rate Limit reached']}
-            }
-        });
-
-        const stormGlass = new StormGlass(mockedAxios);
-        
-        await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
-            'Unexpected error returned by the StormGlass service: Error: {"errors":["Rate Limit reached"]} Code: 429'
-        );
-    });   
-    
+    await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
+      'Unexpected error returned by the StormGlass service: Error: {"errors":["Rate Limit reached"]} Code: 429'
+    );
+  });
 });
--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,104 +1,118 @@
 import { AxiosStatic } from 'axios';
 import { InternalError } from '@src/util/errors/internal-error';
 import { response } from 'express';
+import config, { IConfig } from 'config';
 
 export interface StormGlassPointSource {
-   [key: string]: number;
+  [key: string]: number;
 }
 
 export interface StormGlassPoint {
-    readonly time: string;
-    readonly waveHeight: StormGlassPointSource;
-    readonly waveDirection: StormGlassPointSource;
-    readonly swellDirection: StormGlassPointSource;
-    readonly swellHeight: StormGlassPointSource;
-    readonly swellPeriod: StormGlassPointSource;
-    readonly windDirection: StormGlassPointSource;
-    readonly windSpeed: StormGlassPointSource;
+  readonly time: string;
+  readonly waveHeight: StormGlassPointSource;
+  readonly waveDirection: StormGlassPointSource;
+  readonly swellDirection: StormGlassPointSource;
+  readonly swellHeight: StormGlassPointSource;
+  readonly swellPeriod: StormGlassPointSource;
+  readonly windDirection: StormGlassPointSource;
+  readonly windSpeed: StormGlassPointSource;
 }
 
 export interface StormGlassForecastResponse {
-    hours: StormGlassPoint[];
+  hours: StormGlassPoint[];
 }
 
 export interface ForecastPoint {
-    time: string;
-    waveHeight: number;
-    waveDirection: number;
-    swellDirection: number;
-    swellHeight: number;
-    swellPeriod: number;
-    windDirection: number;
-    windSpeed: number;    
+  time: string;
+  waveHeight: number;
+  waveDirection: number;
+  swellDirection: number;
+  swellHeight: number;
+  swellPeriod: number;
+  windDirection: number;
+  windSpeed: number;
 }
 
 export class ClienteRequestError extends InternalError {
-    constructor(message: string) {
-        const internalMessage = 'Unexpected error when trying to communicate to StormGlass';
-        super(`${internalMessage}: ${message}`);
-    }
+  constructor(message: string) {
+    const internalMessage =
+      'Unexpected error when trying to communicate to StormGlass';
+    super(`${internalMessage}: ${message}`);
+  }
 }
 
 export class StormGlassResponseError extends InternalError {
-    constructor(message: string) {
-        const internalMessage = 'Unexpected error returned by the StormGlass service';
-        super(`${internalMessage}: ${message}`);
-    }
+  constructor(message: string) {
+    const internalMessage =
+      'Unexpected error returned by the StormGlass service';
+    super(`${internalMessage}: ${message}`);
+  }
 }
 
+const stormGlassResourceConfig: IConfig = config.get(
+  'App.resources.StormGlass'
+);
+
 export class StormGlass {
+  readonly stormGlassAPIParams =
+    'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
+  readonly stormeGlassApiSource = 'noaa';
 
-    readonly stormGlassAPIParams = 'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
-    readonly stormeGlassApiSource = 'noaa';
+  constructor(protected request: AxiosStatic) {}
 
-    constructor(protected request: AxiosStatic) {
-    }
-
-    public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
-        try {
-            const response = await this.request.get<StormGlassForecastResponse>(
-                `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormeGlassApiSource}&end=1592113802&lat=${lat}&lng=${lng}`,
-                {
-                    headers: {
-                        Authorization: 'fake-token',
-                    }
-                }
-            );
-            
-            return this.normalizeResponse(response.data);
-        } catch(err) {
-            if(err.response && response.status) {
-                throw new StormGlassResponseError(`Error: ${JSON.stringify(err.response.data)} Code: ${err.response.status}`)
-            }
-            throw new ClienteRequestError(err.message);
+  public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
+    try {
+      const response = await this.request.get<StormGlassForecastResponse>(
+        `${stormGlassResourceConfig.get('apiUrl')}/weather/point?params=${
+          this.stormGlassAPIParams
+        }&source=${
+          this.stormeGlassApiSource
+        }&end=1592113802&lat=${lat}&lng=${lng}`,
+        {
+          headers: {
+            Authorization: stormGlassResourceConfig.get('apiToken'),
+          },
         }
-    }
+      );
 
-    private normalizeResponse(points: StormGlassForecastResponse): ForecastPoint[] {
-        return points.hours
-            .filter(this.isValidPoint.bind(this))
-            .map((point) =>({
-                swellDirection: point.swellDirection[this.stormeGlassApiSource],
-                swellHeight: point.swellHeight[this.stormeGlassApiSource],
-                swellPeriod: point.swellPeriod[this.stormeGlassApiSource],
-                time: point.time,
-                waveDirection: point.waveDirection[this.stormeGlassApiSource],
-                waveHeight: point.waveHeight[this.stormeGlassApiSource],
-                windDirection: point.windDirection[this.stormeGlassApiSource],
-                windSpeed: point.windSpeed[this.stormeGlassApiSource]
-            }));
-    }
-
-    private isValidPoint(point: Partial<StormGlassPoint>): boolean {
-        return !!(
-            point.time &&
-            point.swellDirection?.[this.stormeGlassApiSource] &&
-            point.swellHeight?.[this.stormeGlassApiSource] &&
-            point.swellPeriod?.[this.stormeGlassApiSource] &&
-            point.waveDirection?.[this.stormeGlassApiSource] &&
-            point.waveHeight?.[this.stormeGlassApiSource] &&
-            point.windDirection?.[this.stormeGlassApiSource] &&
-            point.windSpeed?.[this.stormeGlassApiSource]
+      return this.normalizeResponse(response.data);
+    } catch (err) {
+      if (err.response && response.status) {
+        throw new StormGlassResponseError(
+          `Error: ${JSON.stringify(err.response.data)} Code: ${
+            err.response.status
+          }`
         );
+      }
+      throw new ClienteRequestError(err.message);
     }
+  }
+
+  private normalizeResponse(
+    points: StormGlassForecastResponse
+  ): ForecastPoint[] {
+    return points.hours.filter(this.isValidPoint.bind(this)).map((point) => ({
+      swellDirection: point.swellDirection[this.stormeGlassApiSource],
+      swellHeight: point.swellHeight[this.stormeGlassApiSource],
+      swellPeriod: point.swellPeriod[this.stormeGlassApiSource],
+      time: point.time,
+      waveDirection: point.waveDirection[this.stormeGlassApiSource],
+      waveHeight: point.waveHeight[this.stormeGlassApiSource],
+      windDirection: point.windDirection[this.stormeGlassApiSource],
+      windSpeed: point.windSpeed[this.stormeGlassApiSource],
+    }));
+  }
+
+  private isValidPoint(point: Partial<StormGlassPoint>): boolean {
+    return !!(
+      point.time &&
+      point.swellDirection?.[this.stormeGlassApiSource] &&
+      point.swellHeight?.[this.stormeGlassApiSource] &&
+      point.swellPeriod?.[this.stormeGlassApiSource] &&
+      point.waveDirection?.[this.stormeGlassApiSource] &&
+      point.waveHeight?.[this.stormeGlassApiSource] &&
+      point.windDirection?.[this.stormeGlassApiSource] &&
+      point.windSpeed?.[this.stormeGlassApiSource]
+    );
+  }
 }
